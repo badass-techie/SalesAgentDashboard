9ea92128f437560a691a761eb9b9ca6cbe21063e
badass-techie <moses.odhiambo.555@gmail.com>
2024-05-15 12:29:56 +0300

    chore: Update API URL to point to production server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   "production": true,
-  "apiUrl": "https://sales-agent-dashboard-d68blpmmn-moses-projects-215f019f.vercel.app/api"
+  "apiUrl": "https://sales-agent-dashboard-six.vercel.app/api"
 };
